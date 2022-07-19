36b206395f07726e85081c6eef1d992b79d9a2d6
Taruna C <taruna.cotti@gmail.com>
2022-07-19 09:18:57 -0500

    added Loginpage basetest and login page class in framework

---
+++ src/test/java/FunctionalTest/LoginPageTest.java
@@ -0,0 +1,29 @@
+package FunctionalTest;
+
+import Base.BaseTest;
+import PageObjects.LoginPage;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class LoginPageTest extends BaseTest {
+
+@Test
+   private void shouldBeAbleToLoginWithValidUserNameAndPassword()  {
+
+
+    driver.get("https://www.docusign.com/");
+    driver.manage().window().maximize();
+    LoginPage login=new LoginPage(driver);
+    login.clickLoginLink();
+    login.enterMailId("taruna.cotti@gmail.com");
+    login.clickOnNext();
+    login.enterPassword("TestPractice@2");
+    login.clickOnNext();
+   // String loginVerification=driver.findElement(By.xpath("//button[@data-qa='header-HOME-tab-button']")).getText();
+    //System.out.println(loginVerification);
+
+    
+}
+
+
+}
