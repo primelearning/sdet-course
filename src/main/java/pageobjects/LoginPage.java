7cb929c0fe545de93f69349d36772398526abca7
Avishek <avishekbehera87@gmail.com>
2022-09-06 13:13:04 +0530

    Updated LoginPage with indentation

--- src/main/java/pageobjects/LoginPage.java
+++ src/main/java/pageobjects/LoginPage.java
@@ -10,42 +10,48 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public final class LoginPage {
-    private static Logger LOG = LogManager.getLogger(LoginPage.class);
-    private WebDriver localDriver;
+  private static Logger LOG = LogManager.getLogger(LoginPage.class);
+  private WebDriver localDriver;
 
-    //Step1: Implement JAVA coding standards to enable locators and usage of this class
-    public LoginPage(WebDriver driverObject) {
-        this.localDriver = driverObject;
-        PageFactory.initElements(localDriver,this);
-        LOG.info("LoginPage Elements initialized.");
+  //Step1: Implement JAVA coding standards to enable locators and usage of this class
+  public LoginPage(WebDriver driverObject)
+    {
+      this.localDriver = driverObject;
+      PageFactory.initElements(localDriver, this);
+      LOG.info("LoginPage Elements initialized.");
     }
 
-    //Step2: Identify all locators
-    //Locators
-    private String usernameTxt = "#user-name";
-    //Using By Type as Locator
-    private By passwordTxt = By.cssSelector("#password");
-    //Using Page Factory method
-    @FindBy(css = "#login-button") WebElement loginBtn;
-    @FindBy(xpath = "//*[@id=\"login_button_container\"]/div/form/div[3]/h3")
-    WebElement errorMessageInvalidLogin;
+  //Step2: Identify all locators
+  //Locators
+  private String usernameTxt = "#user-name";
+  //Using By Type as Locator
+  private By passwordTxt = By.cssSelector("#password");
+  //Using Page Factory method
+  @FindBy(css = "#login-button")
+  WebElement loginBtn;
+  @FindBy(xpath = "//*[@id=\"login_button_container\"]/div/form/div[3]/h3")
+  WebElement errorMessageInvalidLogin;
 
-    //Step3: Add user behavior methods
-    public void enterUserName(String username) {
-        localDriver.findElement(By.cssSelector(usernameTxt)).sendKeys(username);
+  //Step3: Add user behavior methods
+  public void enterUserName(String username)
+    {
+      localDriver.findElement(By.cssSelector(usernameTxt)).sendKeys(username);
     }
 
-    public void enterPassword(String password) {
-        localDriver.findElement(passwordTxt).sendKeys(password);
+  public void enterPassword(String password)
+    {
+      localDriver.findElement(passwordTxt).sendKeys(password);
     }
 
-    public ProductsPage clickLoginBtn() {
-        loginBtn.click();
-        return new ProductsPage(localDriver);
+  public ProductsPage clickLoginBtn()
+    {
+      loginBtn.click();
+      return new ProductsPage(localDriver);
     }
 
-    public String getErrorMessageForInvalidLogin() {
-       return errorMessageInvalidLogin.getText();
+  public String getErrorMessageForInvalidLogin()
+    {
+      return errorMessageInvalidLogin.getText();
     }
 
 
